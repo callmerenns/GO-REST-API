a653830fcf73ae4070a2f7d8a1be27386892be80
renns19 <mahasiswaedan19@gmail.com>
2024-07-12 17:41:29 +0700

    feat(entity user) update entity user

--- cmd/entity/user.go
+++ cmd/entity/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName string    `gorm:"not null" json:"first_name"`
-	LastName  string    `gorm:"not null" json:"last_name"`
+	FirstName string    `gorm:"not null" json:"firstname"`
+	LastName  string    `gorm:"not null" json:"lastname"`
 	Email     string    `gorm:"not null;unique" json:"email"`
 	Password  string    `gorm:"not null" json:"password"`
 	Role      string    `json:"role"`
